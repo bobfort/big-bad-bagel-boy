0947ffa08f7b0fc80c7808b80668a2ee3f3a9661
John McGuire <spaceconsultant@gmail.com>
2021-09-19 09:09:50 -0500

    Update bigbadbagel.py
    Add auth calls to API in Tweepy

--- bigbadbagel.py
+++ bigbadbagel.py
@@ -13,3 +13,6 @@ def get_twitter_credentials(path):
   return twitter_credentials_dict
   
 twitter_creds = get_twitter_credentials('/content/twitter_credentials.json')
+
+auth = tweepy.OAuthHandler(twitter_creds["API_KEY"], twitter_creds["API_KEY_SECRET"])
+auth.set_access_token(twitter_creds["ACCESS_TOKEN"], twitter_creds["ACCESS_TOKEN_SECRET"])
