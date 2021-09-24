f1e099020b207239b14018440e30ea7981478c6f
John McGuire <spaceconsultant@gmail.com>
2021-09-24 07:53:00 -0500

    Update bigbadbagel.py
    Script to obtain bagel tweets

--- bigbadbagel.py
+++ bigbadbagel.py
@@ -12,10 +12,17 @@ def get_twitter_credentials(path):
       twitter_credentials_dict = json.loads(j.read())
   return twitter_credentials_dict
   
+#set twitter credentials to variable
 twitter_creds = get_twitter_credentials('/content/twitter_credentials.json')
 
+#Authenticate to Twitter
 auth = tweepy.OAuthHandler(twitter_creds["API_KEY"], twitter_creds["API_KEY_SECRET"])
 auth.set_access_token(twitter_creds["ACCESS_TOKEN"], twitter_creds["ACCESS_TOKEN_SECRET"])
 
+#Create API object
 api = tweepy.API(auth, wait_on_rate_limit=True,
     wait_on_rate_limit_notify=True)
+
+#get 1000 most recent bagel tweets
+for tweet in api.search(q="bagel", lang="en", rpp=10, count=1000):
+    print(f"{tweet.user.name}:{tweet.text}")
